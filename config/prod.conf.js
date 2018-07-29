462a8dca7c19d84baa2b9ecdf9f57bdb64b6eae5
trueshura <trueshura@mail.ru>
2018-07-29 16:06:34 +0600

    Add configs

---
+++ config/prod.conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+    network: 0x12882304,
+
+    // some of constants will be injected from prototypes in Factory!
+    constants: {
+
+        // maximum connected peers
+        MAX_PEERS: 10,
+
+        // milliseconds
+        PEER_QUERY_TIMEOUT: 10000
+    }
+};
---
+++ config/test.conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+    network: 0x11111111,
+
+    // some of constants will be injected from prototypes in Factory!
+    constants: {
+
+        // maximum connected peers
+        MAX_PEERS: 10,
+
+        // milliseconds
+        PEER_QUERY_TIMEOUT: 10000
+    }
+};
