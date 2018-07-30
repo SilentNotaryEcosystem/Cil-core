adef9ed3bd8503378658f159aedcac0bcbb63fee
trueshura <trueshura@mail.ru>
2018-07-30 15:50:31 +0600

    Move Wallet init into Factory

--- wallet/wallet.js
+++ wallet/wallet.js
@@ -1,32 +1,30 @@
 'use strict';
 
-const Crypto=require('../crypto/crypto');
+module.exports = (Crypto =>
+        class Wallet {
+            constructor(privateKey) {
+                if (typeof privateKey === 'string') {
+                    this._keyPair = Crypto.keyPairFromPrivate(privateKey, 'hex');
+                } else {
+                    this._keyPair = Crypto.keyPairFromPrivate(privateKey);
+                }
+                this._address = Crypto.getAddress(this._keyPair.getPublic(true, 'hex'));
+            }
 
-class Wallet {
-    constructor(privateKey) {
-        if(typeof privateKey === 'string'){
-            this._keyPair = Crypto.keyPairFromPrivate(privateKey, 'hex');
-        }else{
-            this._keyPair = Crypto.keyPairFromPrivate(privateKey);
+            get address() {
+                return this._address;
+            }
+
+            get publicKey() {
+                return this._keyPair.getPublic(true, 'hex');
+            }
+
+            get privateKey() {
+                return this._keyPair.getPrivate('hex');
+            }
+
+            sign(data) {
+                return Crypto.sign(data, this._keyPair.getPrivate('hex'));
+            }
         }
-        this._address=Crypto.getAddress(this._keyPair.getPublic(true, 'hex'))
-    }
-
-    get address(){
-        return this._address;
-    }
-
-    get publicKey(){
-        return this._keyPair.getPublic(true, 'hex');
-    }
-
-    get privateKey(){
-        return this._keyPair.getPrivate('hex');
-    }
-
-    sign(data){
-        return Crypto.sign(data, this._keyPair.getPrivate('hex'))
-    }
-}
-
-module.exports = Wallet;
+);
