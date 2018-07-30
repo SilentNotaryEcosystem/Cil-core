03524f0e59715bbc16e20083a837e7023b7ed9cf
trueshura <trueshura@mail.ru>
2018-07-30 13:14:42 +0600

    Change Serializer to protobuff

---
+++ network/serializer.js
@@ -0,0 +1,41 @@
+const {BufferReader} = require("protobufjs");
+
+module.exports = MessagesImplementation => {
+    const {MsgCommon, MsgVersion} = MessagesImplementation;
+    return class Serializer {
+        /**
+         *
+         * @param {MessageCommon} message - to send to network
+         * @return {Buffer}
+         */
+        static serialize(message) {
+            return message.encode();
+        }
+
+        /**
+         *
+         * @param {Buffer} buffer - to decode
+         * @param {Boolean} toCommon - when MessageCommon will be enough
+         * @return {Object}
+         */
+        static deSerialize(buffer, toCommon = false) {
+
+            // was message completly downloaded?
+            const buffReader = new BufferReader(buffer);
+            const length = buffReader.int32();
+            if (buffer.length - buffReader.pos !== length) {
+                throw new Error(`Buffer length ${buffer.length} not equal to expected ${length}`);
+            }
+
+            // first we should decide type of message
+            const msg = new MsgCommon(buffer);
+            if (toCommon) return msg;
+
+            // now return properly deserialized message
+            switch (msg.message) {
+                case 'version':
+                    return new MsgVersion(buffer);
+            }
+        }
+    };
+};
