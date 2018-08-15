7c3e148dadf9e8c8bfb9adb1a54011048c6bbc6e
trueshura <trueshura@mail.ru>
2018-08-15 19:39:22 +0600

    Add testStorage

---
+++ storage/testStorage.js
@@ -0,0 +1,23 @@
+const debugLib = require('debug');
+const {sleep} = require('../utils');
+
+const debug = debugLib('storage:');
+
+module.exports = (Constants) =>
+    class Storage {
+        constructor(options) {
+
+            const {arrTestDefinition = []} = options;
+            this._groupDefinitions = new Map(arrTestDefinition);
+        }
+
+        /**
+         *
+         * @return {Promise<*>} Map witnessGroupName -> Array of public keys
+         */
+        async getGroupDefinitions() {
+
+            // TODO: read from DB
+            return this._groupDefinitions;
+        }
+    };
