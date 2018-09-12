29788436024f4c4ab36eb0ba30dcd800dba3dc8b
trueshura <trueshura@mail.ru>
2018-09-12 18:39:56 +0600

    Add RPC test

---
+++ tests/rpc.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const {describe, it} = require('mocha');
+const {assert} = require('chai');
+const sinon = require('sinon').createSandbox();
+
+const factory = require('./testFactory');
+const {createDummyTx} = require('./testUtil');
+
+describe('RPC', () => {
+    before(async function() {
+        this.timeout(15000);
+        await factory.asyncLoad();
+    });
+
+    after(async function() {
+        this.timeout(15000);
+    });
+
+    it('should create instance', async () => {
+        new factory.RPC();
+    });
+
+    it('should emit TX event', (done) => {
+        const rpc = new factory.RPC();
+        const tx = new factory.Transaction(createDummyTx());
+        rpc.on('rpc', ({event, content}) => {
+            assert.isOk(event);
+            assert.isOk(content);
+            assert.equal(event, 'tx');
+            assert.isOk(tx.equals(content));
+            done();
+        });
+
+        rpc.sendRawTx(tx.encode());
+    });
+});
