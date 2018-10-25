58862873e1c00b7a23f81cc13d438dcf225c2f77
Kot Andreev <bottlebo@mail.ru>
2018-10-25 13:37:48 +0700

    Add tests for mempool purgeTxns

--- tests/mempool.spec.js
+++ tests/mempool.spec.js
@@ -2,7 +2,7 @@
 
 const {describe, it} = require('mocha');
 const {assert} = require('chai');
-const {createDummyTx} = require('./testUtil');
+const {sleep, createDummyTx} = require('./testUtil');
 
 const factory = require('./testFactory');
 
@@ -112,4 +112,51 @@ describe('Mempool tests', () => {
         assert.equal(arrTxns.length, 2);
     });
 
+    it('should remove oldest txns if tx qty > MEMPOOL_TX_QTY(2)', async () => {
+        const mempool = new factory.Mempool();
+        const tx1 = new factory.Transaction(createDummyTx());
+        const tx2 = new factory.Transaction(createDummyTx());
+        const tx3 = new factory.Transaction(createDummyTx());
+        mempool.addTx(tx1)
+        mempool.addTx(tx2)
+        mempool.addTx(tx3)
+
+        mempool.purgeTxns()
+
+        assert.isNotOk(mempool.hasTx(tx1.hash()));
+        assert.isOk(mempool.hasTx(tx2.hash()));
+        assert.isOk(mempool.hasTx(tx3.hash()));
+
+    });
+
+    it('should remove oldest txns with age > TX_LIFETIME(5s.)', async () => {
+        const mempool = new factory.Mempool()
+        const tx1 = new factory.Transaction(createDummyTx())
+        const tx2 = new factory.Transaction(createDummyTx())
+        const tx3 = new factory.Transaction(createDummyTx())
+        mempool.addTx(tx1)
+        mempool.addTx(tx2)
+        sleep(6000).then(() =>{
+        mempool.addTx(tx3)
+
+        mempool.purgeTxns()
+
+        assert.isNotOk(mempool.hasTx(tx1.hash()))
+        assert.isNotOk(mempool.hasTx(tx2.hash()))
+        assert.isOk(mempool.hasTx(tx3.hash()))
+        })
+    });
+
+    it('should not remove  txns if tx qty <= 2 and age <= 5s. ', async () => {
+        const mempool = new factory.Mempool();
+        const tx1 = new factory.Transaction(createDummyTx());
+        const tx2 = new factory.Transaction(createDummyTx());
+        mempool.addTx(tx1)
+        mempool.addTx(tx2)
+
+        mempool.purgeTxns()
+
+        assert.isOk(mempool.hasTx(tx1.hash()));
+        assert.isOk(mempool.hasTx(tx2.hash()));
+    });
 });
