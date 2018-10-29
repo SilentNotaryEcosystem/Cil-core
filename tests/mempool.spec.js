2e5ab4a5bb631243751c5842ef0218202955e894
Kot Andreev <bottlebo@mail.ru>
2018-10-29 21:27:52 +0700

    Add tests for purgeOutdated

--- tests/mempool.spec.js
+++ tests/mempool.spec.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const {describe, it} = require('mocha');
-const {assert} = require('chai');
-const {sleep, createDummyTx} = require('./testUtil');
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
+const { sleep, createDummyTx } = require('./testUtil');
 
 const factory = require('./testFactory');
 
 let keyPair;
 
 describe('Mempool tests', () => {
-    before(async function() {
+    before(async function () {
         this.timeout(15000);
         await factory.asyncLoad();
         keyPair = factory.Crypto.createKeyPair();
     });
 
-    after(async function() {
+    after(async function () {
         this.timeout(15000);
     });
 
@@ -112,25 +112,8 @@ describe('Mempool tests', () => {
         assert.equal(arrTxns.length, 2);
     });
 
-    it('should remove oldest txns if tx qty > MEMPOOL_TX_QTY(2)', async () => {
-        const mempool = new factory.Mempool();
-        const tx1 = new factory.Transaction(createDummyTx());
-        const tx2 = new factory.Transaction(createDummyTx());
-        const tx3 = new factory.Transaction(createDummyTx());
-        mempool.addTx(tx1)
-        mempool.addTx(tx2)
-        mempool.addTx(tx3)
-
-        mempool.purgeTxns()
-
-        assert.isNotOk(mempool.hasTx(tx1.hash()));
-        assert.isOk(mempool.hasTx(tx2.hash()));
-        assert.isOk(mempool.hasTx(tx3.hash()));
-
-    });
-
-    it('should remove oldest txns with age > TX_LIFETIME(5s.)', async function ()  {
-        this.timeout(7000)
+    it('should remove oldest txns with age > TX_LIFETIME(5s.)', async function () {
+        this.timeout(10000)
         const mempool = new factory.Mempool()
         const tx1 = new factory.Transaction(createDummyTx())
         const tx2 = new factory.Transaction(createDummyTx())
@@ -140,22 +123,21 @@ describe('Mempool tests', () => {
         await sleep(6000)
         mempool.addTx(tx3)
 
-        mempool.purgeTxns()
+        mempool.purgeOutdated()
 
         assert.isNotOk(mempool.hasTx(tx1.hash()))
         assert.isNotOk(mempool.hasTx(tx2.hash()))
         assert.isOk(mempool.hasTx(tx3.hash()))
-        
     });
 
-    it('should not remove  txns if tx qty <= 2 and age <= 5s. ', async () => {
+    it('should not remove  txns if tx age <= 5s. ', async () => {
         const mempool = new factory.Mempool();
         const tx1 = new factory.Transaction(createDummyTx());
         const tx2 = new factory.Transaction(createDummyTx());
         mempool.addTx(tx1)
         mempool.addTx(tx2)
 
-        mempool.purgeTxns()
+        mempool.purgeOutdated()
 
         assert.isOk(mempool.hasTx(tx1.hash()));
         assert.isOk(mempool.hasTx(tx2.hash()));
