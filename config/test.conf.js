efe7b43edc080f9de6c3b25fc50b9687cc5cc348
Kot Andreev <bottlebo@mail.ru>
2018-11-09 13:07:31 +0700

    Change networkTimeDiff value in seconds

--- config/test.conf.js
+++ config/test.conf.js
@@ -60,7 +60,7 @@ module.exports = {
         },
 
         // maximum time offset for nodes we tolerate
-        networkTimeDiff: 60 * 60 * 1000,
+        networkTimeDiff: 60 * 60,
 
         // how much we suppress creating empty blocks
         WITNESS_HOLDOFF: 15 * 60 * 1000,
