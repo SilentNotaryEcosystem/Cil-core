0ca1c072cb04fc1c95a632dd5cec7b0e4647e855
Kot Andreev <bottlebo@mail.ru>
2018-12-19 15:56:20 +0700

    Fix StorageWrapper call

--- tests/testFactory.js
+++ tests/testFactory.js
@@ -94,8 +94,8 @@ class Factory {
                 this._messageAssemblerImplementation = MessageAssemblerWrapper(this.Serializer);
                 this._transportImplemetation = TransportWrapper(this);
                 this._peerImplementation = PeerWrapper(this);
-                this._peerManagerImplemetation = PeerManagerWrapper(this);
                 this._storageImplementation = StorageWrapper(this);
+                this._peerManagerImplemetation = PeerManagerWrapper(this);
                 this._bftImplementation = BftWrapper(this);
                 this._mempoolImplementation = MempoolWrapper(this);
                 this._rpcImplementation = RpcWrapper(this);
