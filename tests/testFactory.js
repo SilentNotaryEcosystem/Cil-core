00b8795eca8552929b1407839990abe16d044292
trueshura <trueshura@mail.ru>
2019-04-11 17:55:57 +0700

    Change ArrayOfHashes to ArrayOf (add ArrayOfAddresses)

--- tests/testFactory.js
+++ tests/testFactory.js
@@ -58,7 +58,7 @@ const UtxoWrapper = require('../structures/utxo');
 const CoinsWrapper = require('../structures/coins');
 const WitnessGroupDefinition = require('../structures/witnessGroupDefinition');
 const BlockInfoWrapper = require('../structures/blockInfo');
-const ArrayOfHashesWrapper = require('../structures/arrayOfHashes');
+const ArrayOfWrapper = require('../structures/arrayOf');
 const ContractWrapper = require('../structures/contract');
 const TxReceiptWrapper = require('../structures/txReceipt');
 
@@ -89,7 +89,8 @@ class Factory {
                 this._utxoImplementation = UtxoWrapper(this, prototypes);
                 this._witnessGroupDefinition = WitnessGroupDefinition(this, prototypes);
                 this._blockInfo = BlockInfoWrapper(this, prototypes);
-                this._arrayOfHashes = ArrayOfHashesWrapper(this);
+                this._arrayOfHashes = ArrayOfWrapper(32);
+                this._arrayOfAddresses = ArrayOfWrapper(20);
                 this._contract = ContractWrapper(this, prototypes);
                 this._txReceipt = TxReceiptWrapper(this, prototypes);
 
@@ -143,6 +144,10 @@ class Factory {
 
     get ArrayOfHashes() {
         return this._arrayOfHashes;
+    }
+
+    get ArrayOfAddresses() {
+        return this._arrayOfAddresses;
     }
 
     get MainDag() {
