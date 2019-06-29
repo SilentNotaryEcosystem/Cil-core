51ea52e85dcdd052c3020069f0066756adc3dce5
trueshura <trueshura@mail.ru>
2019-06-29 15:04:49 +0600

    Fix Devel sample script

--- runScripts/node.devel/run.node.sh
+++ runScripts/node.devel/run.node.sh
@@ -1,1 +1,1 @@
-sudo DEBUG=node:* NODE_ENV=Devel docker run --restart always -d -p 18222:18222 -p 18223:18223 --env-file sample.node.env --name cil-node-devel cil-core
+sudo docker run --restart always -d -p 18222:18222 -p 18223:18223 --env-file sample.node.env --name cil-node-devel cil-core
