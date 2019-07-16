26656e56c80851a3f6fafdf832214d199e950c18
trueshura <trueshura@mail.ru>
2019-07-16 19:47:20 +0600

    Change docker installation scenarios
    - change docker image name
    - add dev.witness

--- runScripts/node/run.node.sh
+++ runScripts/node/run.node.sh
@@ -1,1 +1,1 @@
-sudo docker run --restart always -d -p 8222:8222 -p 8223:8223 --env-file sample.node.env --name cil-node cil-core
+sudo docker run --restart always -d -p 8222:8222 -p 8223:8223 --env-file sample.node.env --name cil-node trueshura/cil-core-staging:latest
--- runScripts/witness/run.witness.sh
+++ runScripts/witness/run.witness.sh
@@ -1,4 +1,4 @@
 echo "Enter your password for PK (press Ctrl+D when done)"
 cat >temp.pk.password
-sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private -p 8222:8222 -p 8223:8223 --env-file sample.witness.env -e PK_PASSWORD --name cil-witness cil-core
+sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private -p 8222:8222 -p 8223:8223 --env-file sample.witness.env -e PK_PASSWORD --name cil-witness trueshura/cil-core-staging:latest
 rm temp.pk.password
