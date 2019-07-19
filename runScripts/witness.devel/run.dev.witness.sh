f34fb448fa2fc20d29c39ae4742f235405da18c6
trueshura <trueshura@mail.ru>
2019-07-19 20:30:47 +0600

    Remove port mapping for witness containers

--- runScripts/witness.devel/run.dev.witness.sh
+++ runScripts/witness.devel/run.dev.witness.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 echo "Enter your password for PK (press Ctrl+D when done)"
 cat >temp.pk.password
-sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private -p 18222:18222 -p 18223:18223 --env-file sample.witness.env -e DEBUG="node:app, node:messages, node:messages:full" -e NODE_ENV=Devel -e PK_PASSWORD --name cil-witness-devel trueshura/cil-core-staging:latest
+sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private --env-file sample.witness.env -e DEBUG="node:app, node:messages, node:messages:full" -e NODE_ENV=Devel -e PK_PASSWORD --name cil-witness-devel trueshura/cil-core-staging:latest
 rm temp.pk.password
--- runScripts/witness/run.witness.sh
+++ runScripts/witness/run.witness.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 echo "Enter your password for PK (press Ctrl+D when done)"
 cat >temp.pk.password
-sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private -p 8222:8222 -p 8223:8223 --env-file sample.witness.env -e PK_PASSWORD --name cil-witness trueshura/cil-core-staging:latest
+sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private --env-file sample.witness.env -e PK_PASSWORD --name cil-witness trueshura/cil-core-staging:latest
 rm temp.pk.password
