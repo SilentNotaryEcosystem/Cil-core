312c0876ee2a99da969152916d99a25dcf3f8de2
trueshura <trueshura@mail.ru>
2019-07-28 23:48:33 +0200

    Fix docker container names in scripts

--- runScripts/witness/run.witness.sh
+++ runScripts/witness/run.witness.sh
@@ -2,5 +2,5 @@
 
 echo "Enter your password for PK (press Ctrl+D when done)"
 cat >temp.pk.password
-sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private --env-file sample.witness.env -e PK_PASSWORD --name cil-witness trueshura/cil-core-staging:latest
+sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private --env-file sample.witness.env -e PK_PASSWORD --name cil-witness trueshura/cil-core-prod:latest
 rm temp.pk.password
