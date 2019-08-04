3780d5a8bd436fc0e05e20232ed9c02c30809679
trueshura <trueshura@mail.ru>
2019-08-04 23:44:44 +0600

    Fix scripts for Docker deployment

--- runScripts/witness.devel/run.dev.witness.sh
+++ runScripts/witness.devel/run.dev.witness.sh
@@ -2,5 +2,5 @@
 
 echo "Enter your password for PK (press Ctrl+D when done)"
 cat >temp.pk.password
-sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -d -v `pwd`/sample.pk:/app/private --env-file sample.witness.env -e DEBUG="node:app, node:messages, node:messages:full" -e NODE_ENV=Devel -e PK_PASSWORD --name cil-witness-devel trueshura/cil-core-staging:latest
+sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always  -p 18223:18223 -d -v `pwd`/sample.pk:/app/private --env-file sample.witness.env -e DEBUG="node:app, node:messages, node:messages:full" -e NODE_ENV=Devel -e PK_PASSWORD --name cil-witness-devel trueshura/cil-core-staging:latest
 rm temp.pk.password
