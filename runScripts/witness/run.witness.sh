d97434473499259ab9cd50c835d7affc971ccf92
trueshura <trueshura@mail.ru>
2019-08-12 18:07:42 +0600

    Add one more options for node config: "factoryOptions.json"

--- runScripts/witness/run.witness.sh
+++ runScripts/witness/run.witness.sh
@@ -2,5 +2,15 @@
 
 echo "Enter your password for PK (press Ctrl+D when done)"
 cat >temp.pk.password
-sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always -p 8223:8223 -d -v `pwd`/sample.pk:/app/private --env-file sample.witness.env -e PK_PASSWORD --name cil-witness trueshura/cil-core-prod:latest
+
+sudo PK_PASSWORD=`cat temp.pk.password` docker run \
+--restart always \
+-p 8223:8223 \
+-d \
+-v $(pwd)/sample.pk:/app/private \
+-v $(pwd)/factoryOptions.json:/app/factoryOptions.json \
+--env-file sample.witness.env -e PK_PASSWORD \
+--name cil-witness \
+trueshura/cil-core-prod:latest
+
 rm temp.pk.password
