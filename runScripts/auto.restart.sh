623e8431a3da722742107df3855e1d10a5333962
trueshura <trueshura@mail.ru>
2019-10-15 23:09:53 +0600

    Fix auto.restart.sh

--- runScripts/auto.restart.sh
+++ runScripts/auto.restart.sh
@@ -5,7 +5,7 @@
 function checkForUpdate {
   git remote update
   behind=`git status -uno | grep behind | wc -l | awk '{print $1}'`
-  [ "behind" == "1" ] && git pull && kill 6
+  [ $behind$ == "1" ] && git pull && kill 6
 }
 
 while :; do checkForUpdate; sleep 3600; done
