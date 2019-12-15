a0b34fdddb3fa5cf2f7ae4ece3794636de3257bc
trueshura <trueshura@mail.ru>
2019-12-15 21:33:33 +0600

    Change container default behavior to update

--- runScripts/auto.restart.sh
+++ runScripts/auto.restart.sh
@@ -2,10 +2,15 @@
 
 # this script will check for new commits at remote, pull changes, restart container
 
+node index.js
+
+if [[ ${AUTO_UPDATE} == "true" ]]; then
+    while :; do checkForUpdate; sleep 3600; done
+fi
+
+
 function checkForUpdate {
   git remote update
   behind=`git status -uno | grep behind | wc -l | awk '{print $1}'`
-  [ $behind$ == "1" ] && git pull && kill 6
+  [ $behind$ == "1" ] && git pull && kill 7
 }
-
-while :; do checkForUpdate; sleep 3600; done
--- runScripts/witness.devel/run.dev.witness.sh
+++ runScripts/witness.devel/run.dev.witness.sh
@@ -10,7 +10,6 @@ sudo PK_PASSWORD=`cat temp.pk.password` docker run --restart always \
 -p 18223:18223 \
 -d \
 -v $(pwd)/sample.pk:/app/private \
--v $(pwd)/factoryOptions.json:/app/factoryOptions.json \
 --env-file sample.witness.env -e DEBUG="node:app, node:messages, node:messages:full" -e NODE_ENV=Devel -e PK_PASSWORD \
 --name $CONTAINER_NAME \
 trueshura/cil-core-staging:latest
