f93a13a42800b661077d654695b8a4bdc9c0363a
trueshura <trueshura@mail.ru>
2019-12-18 13:05:57 +0600

    Fix autoupdate script

--- runScripts/auto.restart.sh
+++ runScripts/auto.restart.sh
@@ -2,15 +2,19 @@
 
 # this script will check for new commits at remote, pull changes, restart container
 
-node index.js
+function checkForUpdate {
+  echo "Checking for update ..."
+  git remote update
+  behind=`git status -uno | grep behind | wc -l | awk '{print $1}'`
+  [[ $behind == "1" ]] && git pull && exit 0
+}
+
+node index.js &
 
 if [[ ${AUTO_UPDATE} == "true" ]]; then
-    while :; do checkForUpdate; sleep 3600; done
+    while :; do checkForUpdate; sleep 30m; done
+else
+    while :; do sleep 60m; done
 fi
 
 
-function checkForUpdate {
-  git remote update
-  behind=`git status -uno | grep behind | wc -l | awk '{print $1}'`
-  [[ $behind == "1" ]] && git pull && pkill -o bash
-}
